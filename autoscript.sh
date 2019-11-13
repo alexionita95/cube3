2cc07f8081734832db5cfce0448e683c2aba6f47
alexionita95 <ionita.alexandru95@gmail.com>
2019-11-13 17:51:50 +0200

    Fixed syntax error in autoscript.sh.

--- autoscript.sh
+++ autoscript.sh
@@ -9,24 +9,29 @@ ACTION='\033[1;90m'
 
 killProcess()
 {
-if pgrep -x $proc_name > /dev/null; then
-echo
-else
+if pgrep $proc_name
+then
+echo ""
 pkill $proc_name
+
+else
+echo ""
 fi
 }
 keepAlive()
 {
-if pgrep -x $proc_name > /dev/null; then
-echo
+if pgrep $proc_name
+then
+echo ""
 else
 cd ../
-./sauerbraten_unix -d2 &
+./sauerbraten_unix &
 cd src
 fi
 }
 
  while true; do 
+ echo -e ${ACTION}Checking Git repo
  echo -e =======================${NOCOLOR}
  BRANCH=$(git rev-parse --abbrev-ref HEAD)
  git fetch
@@ -42,6 +47,9 @@ fi
  echo -e ${ACTION}Building new sources...
  make install
  echo -e ${ACTION}Done updating.
+ 
+ else
+   echo -e ${FINISHED}Current branch is up to date with origin/master.${NOCOLOR}
  fi
  keepAlive
 done
